5b5754f2b4279f02ff0333330f4ef4a4aa132ad4
Franco Ceballos <franco.ceballos2005@gmail.com>
2024-10-17 12:27:30 -0300

    Added moving background

--- Assets/BackgroundScript.cs
+++ Assets/BackgroundScript.cs
@@ -7,14 +7,19 @@ public class BackgroundScript : MonoBehaviour
     public GameObject background;
     public Rigidbody2D backgroundRB;
     public float speed = 0.5f;
+    public float resetPositionX = 28.11f;
+    public float offscreenPositionX = -30f;
     void Start()
     {
-        
+        backgroundRB.velocity = Vector2.left * speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        backgroundRB.velocity = Vector2.left * speed;
+        if (background.transform.position.x < offscreenPositionX)
+        {
+            background.transform.position = new Vector3(resetPositionX, background.transform.position.y, background.transform.position.z);
+        }
     }
 }
